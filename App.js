878fb281a5a0a9c2714f91e746d4a58325de629f
Mohak Sharma <mohak327@gmail.com>
2021-07-10 11:53:48 +0530

    image picker

--- App.js
+++ App.js
@@ -1,31 +1,69 @@
-import React , {useState} from "react";
+import React, { useState, useEffect } from 'react'
 
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
-import LoginScreen from './app/screens/LoginScreen';
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import ViewImageScreen from './app/screens/ViewImageScreen'
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen'
+import WelcomeScreen from './app/screens/WelcomeScreen'
+import MessagesScreen from './app/screens/MessagesScreen'
+import AccountScreen from './app/screens/AccountScreen'
+import ListingsScreen from './app/screens/ListingsScreen'
+import LoginScreen from './app/screens/LoginScreen'
+import ListingEditScreen from './app/screens/ListingEditScreen'
 
-import Card from './app/components/Card';
-import Screen from './app/components/Screen';
-import Icon from './app/components/Icon';
-import ListItem from './app/components/lists/ListItem';
-import AppTextInput from './app/components/AppTextInput';
-import AppPicker from './app/components/AppPicker';
+import Screen from './app/components/Screen'
+
+import * as ImagePicker from 'expo-image-picker'
+import * as Permissions from 'expo-permissions'
+import { Button, Image } from 'react-native';
+import ImageInput from './app/components/ImageInput';
 
 export default function App() {
+	const [imageUri, setImageUri] = useState();
 
-  return (
-    //  <ViewImageScreen />
-    //  <WelcomeScreen />
-    // <ListingDetailsScreen/>
-    // <MessagesScreen />
-    // <AccountScreen />
-	// <ListingsScreen />
-	// <LoginScreen />
-	<ListingEditScreen />
-  );
+	const requestPermission = async () => {
+		const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+        if (!granted) {
+          	alert('Sorry, you need camera roll permissions to make upload items!');
+        }
+	}
+
+// async function cannot be passed in a use hook. so we will return a function promise
+// return promise function
+	useEffect(() => {
+		requestPermission();
+	}, [])
+
+	const selectImage= async() => {
+		try {
+			const result = await ImagePicker.launchImageLibraryAsync();
+
+			if(!result.cancelled) {
+				setImageUri(result.uri);
+			}
+
+		} catch (error) {
+			console.log("Error reading an image", error)
+		}
+	}
+
+	return (
+		<Screen>
+			{/* <Button title="Image" onPress={selectImage}/>
+			<Image
+				source={{ uri: imageUri}}
+				style={{ width: 150, height: 150 }}/> */}
+			<ImageInput
+				imageUri={imageUri}
+				onChangeImage={(uri) => setImageUri(uri)}
+			/>
+		</Screen>
+
+		//  <ViewImageScreen />
+		//  <WelcomeScreen />
+		// <ListingDetailsScreen/>
+		// <MessagesScreen />
+		// <AccountScreen />
+		// <ListingsScreen />
+		// <LoginScreen />
+		// <ListingEditScreen />
+	)
 }
---
+++ app/components/ImageInput.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { View, StyleSheet, Image, TouchableWithoutFeedback, Alert } from 'react-native';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker'
+
+import colors from "../config/colors";
+
+function ImageInput({ imageUri, onChangeImage }) {
+
+    const handlePress = () => {
+        if(!imageUri)
+            selectImage();
+        else
+        // Alert component in React Native has 3 arguments passed in it:
+            // Alert heading
+            // Alert message
+            // An array of alert button elements with properties
+            Alert.alert('Delete', 'Are you sure you want to delete this iamge?', [
+                { text: 'Yes', onPress: () => onChangeImage(null)},
+                { text: 'No'},
+            ]);
+    };
+
+    const selectImage = async() => {
+		try {
+			const result = await ImagePicker.launchImageLibraryAsync({
+            // specify media types
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            // quality ranges between 0(lowest) to 1(highest) quality
+                quality: 0.5
+            });
+
+			if(!result.cancelled) {
+				onChangeImage(result.uri);
+			}
+
+		} catch (error) {
+			console.log("Error reading an image", error)
+		}
+	}
+
+    return (
+        <TouchableWithoutFeedback onPress={handlePress}>
+            <View style={styles.container}>
+                {!imageUri &&
+                    <MaterialCommunityIcons
+                        name="camera"
+                        size={40}
+                        color={colors.medium}
+                    />
+                }
+
+                {imageUri &&
+                    <Image
+                        source={{uri: imageUri}}
+                        style={styles.image}
+                    />
+                }
+            </View>
+        </TouchableWithoutFeedback>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        backgroundColor: colors.light,
+        borderRadius: 15,
+        height: 150,
+        width: 150,
+        justifyContent: 'center',
+        overflow: "hidden"
+    },
+    image: {
+        height: '100%',
+        width: '100%',
+    }
+})
+
+export default ImageInput;
